cd7592d68c635c67a75fc34aab2b22ef954fbbcb
Ernesto Echeverría González <ernestoechegon@gmail.com>
2022-09-13 14:11:01 +0100

    feat(game): se añade el proyecto

---
+++ Assets/ARCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCursor : MonoBehaviour
+{
+    public GameObject cursorChildObject;
+    public GameObject objectToPlace;
+    public ARRaycastManager raycastManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
